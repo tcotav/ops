5cf059eb6130ac046f01fe29ffd885f775961a79
James Francis <tcotav@gmail.com>
2013-08-29 16:22:20 -0700

    added order sub-items and pricing per item to report

--- getItunesOrdersFromGmail.py
+++ getItunesOrdersFromGmail.py
@@ -1,28 +1,58 @@
-import email, getpass, imaplib, datetime
+import email, getpass, imaplib, datetime, re
 
 # ref: http://stackoverflow.com/questions/7314942/python-imaplib-to-get-gmail-inbox-subjects-titles-and-sender-name?rq=1
 
 user = raw_input("Enter your GMail username --> ")
 gmailLabel="itunes"
-NUM_DAYS=60
+NUM_DAYS=30
 
 def process(str):
     lines=str.split("\r\n")
     orderNum=orderDate=orderTotal=0
+    order={}
+    orderItems=[]
+    orderItem=None
+    state=0
+    pLine1=None
+    pLine2=None
+    linecount=0
     for l in lines:
-        l=l.strip()
-        if l[:9] == "Order Num":
-            items=l.split(" ")
-            orderNum=items[2]
-        elif l[:12] == "Receipt Date":
-            items=l.split(" ")
-            orderDate=items[2]
-        elif l[:11] == "Order Total":
-            items=l.split(" ")
-            orderTotal=items[2]
-            # we know this is last item we want from email so exist loop
-            break
-    return (orderDate, "%s,%s,%s" % (orderNum, orderDate, orderTotal))
+        try: 
+            linecount +=1
+            if len(l) ==0: continue
+            l=l.strip()
+            if l[:4] == "----" and state==3: # closing ---------wall
+                state=5
+                break
+            elif l[:9] == "Order Num":
+                items=l.split(" ")
+                order["id"] = items[2]
+            elif l[:12] == "Receipt Date":
+                items=l.split(" ")
+                order["date"] = items[2]
+            elif l[:11] == "Order Total":
+                items=l.split(" ")
+                order["total"] = items[2]
+                # we know this is last item we want from email so exist loop
+            elif l[:4] == "ype ":
+                state=1
+            elif l[:4] == "----" and state==1: # opening ---------wall
+                state=2
+            elif l[:4] == "----" and state==2: # opening. line 2 ---------wall
+                state=3
+            elif l[0] != '-' and (state==3):
+                # first line of purchase
+                pLine1=re.sub( '\s\s+', '|', l ).split('|')
+                state=4
+            elif l[0] != '-' and (state==4):
+                pLine2=re.sub( '\s\s+', '|', l ).split('|')
+                orderItems.append([pLine1[0].replace(","," "), pLine2[1]])
+                state=3
+        except:
+            print "Error in line"
+            print l
+    order['items'] = orderItems
+    return order
 
 def getMail():
     # password prompt
@@ -61,13 +91,16 @@ def getMail():
                     continue
                   payload += part.get_payload()
 
-        k, v=process(payload)
-        msgDict[k] = v
+        o=process(payload)
+        msgDict[o["date"]] = o 
 
     keys=msgDict.keys()
     keys.sort(reverse=True)
     for k in keys:
-        print msgDict[k]
+        o=msgDict[k]
+        print "%s,%s,,,%s" % (o['date'], o['id'], o['total'])
+        for oi in o['items']:
+            print ",,%s,%s" % (oi[0], oi[1])
 
 if __name__ == "__main__":
     test=False
@@ -76,7 +109,11 @@ if __name__ == "__main__":
         f=open("data.txt")
         s=""
         s=" ".join(f.readlines())
-        process(s)
+        o=process(s)
+        print "DATE,ID,Item Name, Item Charge, TOTAL"
+        print "%s,%s,%s" % (o['date'], o['id'], o['total'])
+        for oi in o['items']:
+            print ",%s,%s" % (oi[0], oi[1])
         f.close()
 
     getMail()
